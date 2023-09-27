720f8723fb26a7bef80a71e4ecd5b60eb3c736cb
alxmart <alxmartinez@gmail.com>
2023-09-27 12:03:26 -0300

    Creating Adapter

--- app/src/main/java/com/luizafmartinez/recyclerviewdozero/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/recyclerviewdozero/MainActivity.kt
@@ -2,10 +2,29 @@ package com.luizafmartinez.recyclerviewdozero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvConversas: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvConversas = findViewById(R.id.rv_conversas)
+
+        //Adaptador de Dados
+        rvConversas.adapter = ConversasAdapter()
+
+        //Gerenciador de Layout
+        rvConversas.layoutManager = LinearLayoutManager(
+            this, RecyclerView.VERTICAL, false)
+
+
+
+
+
     }
 }
